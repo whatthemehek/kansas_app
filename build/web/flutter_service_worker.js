09a871da78375ac72f5bdefe79a598714cce03d3
mook <mehek@live.com>
2020-10-16 01:18:22 -0700

    trying to fix icons pt 2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -114,8 +114,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1c983abf05a1fa5267b495d307123acb",
-"/": "1c983abf05a1fa5267b495d307123acb",
+"index.html": "c742d01f7233a54cdc0013dc85392c18",
+"/": "c742d01f7233a54cdc0013dc85392c18",
 "main.dart.js": "ece10216f68d76b10bf5af7426871999",
 "manifest.json": "5c363b8fb1aa9ef207b2567ae5540884"
 };
