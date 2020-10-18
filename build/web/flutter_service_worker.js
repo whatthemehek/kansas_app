07fa90bff1834fa25f199392587a7e05d97e3454
mook <mehek@live.com>
2020-10-18 15:19:58 -0700

    updating changes from original site, reformatting code to multiple dart files, fixing draggable bug

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -114,9 +114,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c742d01f7233a54cdc0013dc85392c18",
-"/": "c742d01f7233a54cdc0013dc85392c18",
-"main.dart.js": "ece10216f68d76b10bf5af7426871999",
+"index.html": "e2e29bda252cbf973f6db61b1e84ade3",
+"/": "e2e29bda252cbf973f6db61b1e84ade3",
+"main.dart.js": "2757153a116499bd7b587128f5870a65",
 "manifest.json": "5c363b8fb1aa9ef207b2567ae5540884"
 };
 
